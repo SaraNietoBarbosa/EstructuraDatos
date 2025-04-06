ab3f909eac6f4990e75c35ce92fb8b7d654f1976
SaraNieto <saramariananietobarbosa@gmail.com>
2025-04-06 18:26:23 -0500

    Actualizacion del archivo

--- EstructuraDatos.php
+++ EstructuraDatos.php
@@ -1,33 +1,47 @@
 <?php
 
 $maquillaje = ["gloss", "sombras", "blush", "labial"];
-
-echo "Lista inicial de maquillaje:\n";
+echo "Lista inicial de maquillaje\n";
 print_r($maquillaje);
 
 
 $maquillaje[] = "rimel";
-echo "Se agrega rimel a la lista\n";
+echo "\nAgregar rimel al array\n";
+print_r($maquillaje);
+
+
+$maquillaje[2] = "lapiz";
+echo "\nreemplazar el elemento 2 por lapiz\n";
 print_r($maquillaje);
 
 
 $eliminar = "gloss";
 $llave = array_search($eliminar, $maquillaje);
-
 if ($llave !== false) {
     unset($maquillaje[$llave]);
-    echo "Se elimina $eliminar de la lista\n";
-} 
-print_r($maquillaje);
+    echo "Eliminar el elemento gloss del array\n";
+    print_r($maquillaje);
+}
 
 
-$maquillaje[2] = "lapiz";
-echo "Se reemplaza el elemento en la posición 2 con lapiz\n";
-print_r($maquillaje);
-
-
-$subset = array_slice($maquillaje, 1, 3); // Desde la posición 1, toma 3 elementos
-echo "Elementos seleccionados:\n";
+$subset = array_slice($maquillaje, 1, 3);
+echo "\nSe extraen 3 elementos desde la posicion 1\n";
 print_r($subset);
 
-?>
+
+$maquillaje2 = ["polvo", "base", "corrector"];
+$combinado = array_merge($maquillaje, $maquillaje2);
+echo "\nSe combinan dos arrays de maquillaje\n";
+print_r($combinado);
+
+
+sort($combinado);
+echo "\nOrdenar la lista alfabeticamente\n";
+print_r($combinado);
+
+
+$filtrado = array_filter($combinado, function($item) {
+    return strlen($item) > 5;
+});
+echo "\nFiltrar elementos del array con mas de 5 caracteres\n";
+print_r($filtrado);
